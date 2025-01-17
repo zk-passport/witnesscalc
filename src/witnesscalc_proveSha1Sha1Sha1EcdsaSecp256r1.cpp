b5b9ac424255fc2414cba689b14be94f941398a5
ayman <aymanshaik1015@gmail.com>
2025-01-17 14:58:35 +0530

    feat: add sha1 sha1 sha1 secp256r1

---
+++ src/witnesscalc_proveSha1Sha1Sha1EcdsaSecp256r1.cpp
@@ -0,0 +1,15 @@
+#include "witnesscalc_proveSha1Sha1Sha1EcdsaSecp256r1.h"
+#include "witnesscalc.h"
+
+int
+witnesscalc_proveSha1Sha1Sha1EcdsaSecp256r1(
+    const char *circuit_buffer,  unsigned long  circuit_size,
+    const char *json_buffer,     unsigned long  json_size,
+    char       *wtns_buffer,     unsigned long *wtns_size,
+    char       *error_msg,       unsigned long  error_msg_maxsize)
+{
+    return CIRCUIT_NAME::witnesscalc(circuit_buffer, circuit_size,
+                       json_buffer,   json_size,
+                       wtns_buffer,   wtns_size,
+                       error_msg,     error_msg_maxsize);
+}
